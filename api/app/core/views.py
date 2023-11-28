f3f15059a217655d3b4e7726fb82b8abaa486b49
dameyerdave <dameyerdave@gmail.com>
2023-11-28 16:37:26 +0100

    [FEAT] clone can now filter members with not accession

--- api/app/core/views.py
+++ api/app/core/views.py
@@ -1,5 +1,3 @@
-import threading
-import time
 import yaml
 from datetime import datetime as dt
 from os.path import join, isfile
@@ -9,7 +7,6 @@ from rest_framework.response import Response
 from rest_framework.exceptions import ValidationError, APIException
 from django.conf import settings
 from django.utils import timezone as tz
-from core import log
 from .serializers import (
     JobSerializer,
     AnalysisJobSerializer,
@@ -17,56 +14,10 @@ from .serializers import (
     AnalysisFileSerializer,
 )
 from .models import Job, AnalysisJob
-from .ena_helpers import ena_upload, webin_upload, apply_template, SCHEMAS
+from .ena_helpers import apply_template, SCHEMAS
 from .helpers import merge
 from ena_upload import ena_upload as ena
 from django.utils.translation import gettext_lazy as _
-
-
-###
-# Background thread to upload the jobs
-###
-def upload_process():
-    while True:
-        # log.debug("Handling queued jobs...")
-        queued_jobs = Job.objects.filter(status="QUEUED")
-        for queued_job in queued_jobs:
-            log.info(f"Handling queued job {queued_job}...")
-            try:
-                queued_job.status = "RUNNING"
-                queued_job.save()
-                ena_upload(queued_job)
-            except Exception as ex:
-                queued_job.status = "ERROR"
-                queued_job.raw_result = ex
-                queued_job.save()
-                log.exception(ex)
-                log.exception(ex.__traceback__)
-
-        queued_analysisjobs = AnalysisJob.objects.filter(status="QUEUED")
-        for queued_analysisjob in queued_analysisjobs:
-            log.info(f"Handling queued analysis job {queued_analysisjob}...")
-            try:
-                if queued_analysisjob.analysisjob_files.count() > 0:
-                    queued_analysisjob.status = "RUNNING"
-                    queued_analysisjob.save()
-                    webin_upload(queued_analysisjob)
-                else:
-                    log.warning(
-                        f"Analysis job {queued_analysisjob} has no assigned files!"
-                    )
-            except Exception as ex:
-                queued_analysisjob.status = "QUEUED"
-                queued_analysisjob.raw_result = ex
-                queued_analysisjob.save()
-                log.exception(ex)
-                log.exception(ex.__traceback__)
-        time.sleep(settings.ENA_UPLOAD_FREQ_SECS)
-
-
-upload_thread = threading.Thread(target=upload_process, args=(), kwargs={})
-upload_thread.setDaemon(True)
-upload_thread.start()
 
 
 ###
@@ -158,7 +109,7 @@ class JobViewset(
 
     def __release_cancel(self, request, pk, action: str):
         job = Job.objects.get(pk=pk)
-        new_job = job.clone(request.user, action)
+        new_job = job.clone(request.user, action, filter_none_accession=True)
         new_job.save()
         serializer = JobSerializer(instance=new_job, context={"request": request})
         return Response(serializer.data)
