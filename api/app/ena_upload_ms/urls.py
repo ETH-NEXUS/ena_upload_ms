6b9855c3f3fac46a0e8bbdce610ffd7158a6d634
dameyerdave <dameyerdave@gmail.com>
2024-01-04 14:14:16 +0100

    [FIX] yet another static url pattern

--- api/app/ena_upload_ms/urls.py
+++ api/app/ena_upload_ms/urls.py
@@ -14,15 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import re
+from django.views.static import serve
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
 from django.conf import settings
 from drf_auto_endpoint.router import router
 
-# from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from rest_framework.authtoken import views as authtoken_views
-from django.conf.urls.static import static
-from django.contrib.staticfiles.views import serve
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularRedocView,
@@ -50,7 +49,10 @@ urlpatterns = [
 ]
 
 if not settings.DEBUG:
-    # urlpatterns += staticfiles_urlpatterns()
-    urlpatterns += static(
-        settings.STATIC_URL, view=serve, document_root=settings.STATIC_ROOT
-    )
+    urlpatterns += [
+        re_path(
+            r"^%s(?P<path>.*)$" % re.escape(settings.STATIC_URL.lstrip("/")),
+            serve,
+            kwargs={"document_root": settings.STATIC_ROOT},
+        ),
+    ]
