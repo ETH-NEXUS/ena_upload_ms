4cace0e3eaa91e8b0e192ea7edb92526138af9cd
dameyerdave <dameyerdave@gmail.com>
2024-01-04 11:15:10 +0100

    [CHA] different way to expose static files

--- api/app/ena_upload_ms/urls.py
+++ api/app/ena_upload_ms/urls.py
@@ -18,8 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from drf_auto_endpoint.router import router
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+# from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from rest_framework.authtoken import views as authtoken_views
+from django.conf.urls.static import static
+from django.contrib.staticfiles.views import serve
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularRedocView,
@@ -47,4 +50,7 @@ urlpatterns = [
 ]
 
 if not settings.DEBUG:
-    urlpatterns += staticfiles_urlpatterns()
+    # urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(
+        settings.STATIC_URL, view=serve, document_root=settings.STATIC_ROOT
+    )
