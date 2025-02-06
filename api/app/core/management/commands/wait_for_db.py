372f2e30525217f2c741ac3b4c5613f6253daf68
dameyerdave <dameyerdave@gmail.com>
2025-02-06 15:09:58 +0100

    [FEAT] db (dev) will be created if it does not exist

--- api/app/core/management/commands/wait_for_db.py
+++ api/app/core/management/commands/wait_for_db.py
@@ -1,7 +1,9 @@
 import time
+
+import psycopg2
+from django.conf import settings
 from django.core.management.base import BaseCommand
-from django.db import connections
-from django.db.utils import OperationalError
+from django.db import OperationalError, connections
 
 
 class Command(BaseCommand):
@@ -18,3 +20,39 @@ class Command(BaseCommand):
                 time.sleep(1)
 
         self.stdout.write(self.style.SUCCESS("Database available!"))
+
+        # Check if the database exists, and create it if it does not
+        self.create_database_if_not_exists()
+
+    def create_database_if_not_exists(self):
+        db_name = settings.DATABASES["default"]["NAME"]
+        db_user = settings.DATABASES["default"]["USER"]
+        db_password = settings.DATABASES["default"]["PASSWORD"]
+        db_host = settings.DATABASES["default"]["HOST"]
+        db_port = settings.DATABASES["default"]["PORT"]
+
+        conn = psycopg2.connect(
+            dbname="postgres",
+            user=db_user,
+            password=db_password,
+            host=db_host,
+            port=db_port,
+        )
+        conn.autocommit = True
+        cursor = conn.cursor()
+
+        cursor.execute(f"SELECT 1 FROM pg_database WHERE datname='{db_name}'")
+        exists = cursor.fetchone()
+        if not exists:
+            self.stdout.write(f"Database '{db_name}' does not exist. Creating...")
+            cursor.execute(f"CREATE DATABASE {db_name}")
+            self.stdout.write(
+                self.style.SUCCESS(f"Database '{db_name}' created successfully!")
+            )
+        else:
+            self.stdout.write(
+                self.style.SUCCESS(f"Database '{db_name}' already exists.")
+            )
+
+        cursor.close()
+        conn.close()
