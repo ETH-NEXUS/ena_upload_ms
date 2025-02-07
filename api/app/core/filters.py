69e1667fa8126e8ee4f572ad4e5a39a1ea2ac3fa
dameyerdave <dameyerdave@gmail.com>
2025-02-07 11:53:56 +0100

    [FEAT] adds additional filters for aliases

--- api/app/core/filters.py
+++ api/app/core/filters.py
@@ -9,10 +9,22 @@ class JobFilterSet(filters.FilterSet):
     def accession_filter(queryset, name, value):
         return queryset.filter(**{f"result__{name}__accession__icontains": value})
 
+    def alias_filter(queryset, name, value):
+        return queryset.filter(**{f"submission__{name}__icontains": value})
+
     files = filters.CharFilter(lookup_expr="icontains")
+
+    # Accession filters
     sample = filters.CharFilter(label="Sample", method=accession_filter)
     experiment = filters.CharFilter(label="Experiment", method=accession_filter)
     run = filters.CharFilter(label="Run", method=accession_filter)
+
+    # Alias filters
+    sample__alias = filters.CharFilter(label="Sample alias", method=alias_filter)
+    experiment__alias = filters.CharFilter(
+        label="Experiment alias", method=alias_filter
+    )
+    run__alias = filters.CharFilter(label="Run alias", method=alias_filter)
 
     class Meta:
         model = Job
